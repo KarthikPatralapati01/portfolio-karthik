e640dd827515bf17eedaf6aa4c06b4856c31afab
Venkata Karthik Patralapati <karthikpatralapati.official@gmail.com>
2025-04-26 16:51:19 -0700

    Update SocialIcons.tsx

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -6,14 +6,14 @@ import Link from "next/link";
 export default function SocialIcons() {
   return (
     <div className="absolute top-5 left-5 flex space-x-5 z-50">
-      <Link href="https://github.com/KarthikPatralapati01" target="_blank">
-        <FaGithub className="text-gray-400 hover:text-white transition duration-300 cursor-pointer text-2xl" />
+      <Link href="https://github.com/KarthikPatralapati01" target="_blank" rel="noopener noreferrer">
+        <FaGithub className="text-gray-400 hover:text-emerald-400 transition duration-300 cursor-pointer text-2xl hover:scale-125" />
       </Link>
-      <Link href="https://www.linkedin.com/in/pvk04/" target="_blank">
-        <FaLinkedin className="text-gray-400 hover:text-white transition duration-300 cursor-pointer text-2xl" />
+      <Link href="https://www.linkedin.com/in/pvk04/" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin className="text-gray-400 hover:text-emerald-400 transition duration-300 cursor-pointer text-2xl hover:scale-125" />
       </Link>
-      <Link href="https://leetcode.com/u/karthikpatralapati/" target="_blank">
-        <FaCode className="text-gray-400 hover:text-white transition duration-300 cursor-pointer text-2xl" />
+      <Link href="https://leetcode.com/venkatakarthik04/" target="_blank" rel="noopener noreferrer">
+        <FaCode className="text-gray-400 hover:text-emerald-400 transition duration-300 cursor-pointer text-2xl hover:scale-125" />
       </Link>
     </div>
   );
