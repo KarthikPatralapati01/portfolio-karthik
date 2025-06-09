a14072f94bccf708a7833b93d81bb157e44b1099
Venkata Karthik Patralapati <karthikpatralapati.official@gmail.com>
2025-06-08 20:28:53 -0700

    Update certifications.ts

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -1,63 +1,62 @@
 export const certificationsData = [
-    {
+  {
     title: "AWS Certified Solutions Architect – Associate",
     issuer: "Amazon Web Services (AWS)",
     link: "https://www.credly.com/badges/d9e087ad-853c-463e-bbbb-3ba663f4caa8",
-    },
-    {
-      title: "5 Day Generative AI Intensive by Google",
-      issuer: "Google (Kaggle Certification)",
-      link: "https://www.kaggle.com/certification/badges/karthikpatralapati01/96",
-    },
-    {
-      title: "Oracle Cloud Infrastructure 2024 Generative AI Certified Professional",
-      issuer: "Oracle Cloud Certification",
-      link: "https://catalog-education.oracle.com/ords/certview/sharebadge?id=441CA5BBA52595E4DA2337A8C9279FA233478F3032EC19B2F9E5C854E26254E7",
-    },
-    {
-      title: "IBM Excel for Data Analytics",
-      issuer: "Coursera (IBM)",
-      link: "https://www.linkedin.com/in/pvk04/details/certifications/",
-    },
-    {
-      title: "IBM Databases and SQL for Data Science with Python",
-      issuer: "Coursera (IBM)",
-      link: "https://www.linkedin.com/in/pvk04/details/certifications/",
-    },
-    {
-      title: "Programming for Everybody (Python)",
-      issuer: "Coursera",
-      link: "https://www.linkedin.com/in/pvk04/details/certifications/",
-    },
-    {
-      title: "VLSI Design Methodologies",
-      issuer: "Maven Silicon",
-      link: "https://www.linkedin.com/in/pvk04/details/certifications/",
-    },
-    {
-      title: "Electric Industry Operations and Markets",
-      issuer: "Duke University (Coursera)",
-      link: "https://www.linkedin.com/in/pvk04/details/certifications/",
-    },
-    {
-      title: "Electric Power Systems",
-      issuer: "University at Buffalo, SUNY (Coursera)",
-      link: "https://www.linkedin.com/in/pvk04/details/certifications/",
-    },
-    {
-      title: "Google Foundations: Data, Data Everywhere",
-      issuer: "Coursera (Google)",
-      link: "https://www.linkedin.com/in/pvk04/details/certifications/",
-    },
-    {
-      title: "Intelligent Machining",
-      issuer: "University at Buffalo, SUNY (Coursera)",
-      link: "https://www.linkedin.com/in/pvk04/details/certifications/",
-    },
-    {
-      title: "Neural Networks and Deep Learning",
-      issuer: "Coursera (DeepLearning.AI)",
-      link: "https://www.linkedin.com/in/pvk04/details/certifications/",
-    },
-  ];
-  
+  },
+  {
+    title: "5 Day Generative AI Intensive by Google",
+    issuer: "Google (Kaggle Certification)",
+    link: "https://www.kaggle.com/certification/badges/karthikpatralapati01/96",
+  },
+  {
+    title: "Oracle Cloud Infrastructure 2024 Generative AI Certified Professional",
+    issuer: "Oracle Cloud Certification",
+    link: "https://catalog-education.oracle.com/ords/certview/sharebadge?id=441CA5BBA52595E4DA2337A8C9279FA233478F3032EC19B2F9E5C854E26254E7",
+  },
+  {
+    title: "IBM Excel for Data Analytics",
+    issuer: "Coursera (IBM)",
+    link: "https://www.linkedin.com/in/pvk04/details/certifications/",
+  },
+  {
+    title: "IBM Databases and SQL for Data Science with Python",
+    issuer: "Coursera (IBM)",
+    link: "https://www.linkedin.com/in/pvk04/details/certifications/",
+  },
+  {
+    title: "Programming for Everybody (Python)",
+    issuer: "Coursera",
+    link: "https://www.linkedin.com/in/pvk04/details/certifications/",
+  },
+  {
+    title: "VLSI Design Methodologies",
+    issuer: "Maven Silicon",
+    link: "https://www.linkedin.com/in/pvk04/details/certifications/",
+  },
+  {
+    title: "Electric Industry Operations and Markets",
+    issuer: "Duke University (Coursera)",
+    link: "https://www.linkedin.com/in/pvk04/details/certifications/",
+  },
+  {
+    title: "Electric Power Systems",
+    issuer: "University at Buffalo, SUNY (Coursera)",
+    link: "https://www.linkedin.com/in/pvk04/details/certifications/",
+  },
+  {
+    title: "Google Foundations: Data, Data Everywhere",
+    issuer: "Coursera (Google)",
+    link: "https://www.linkedin.com/in/pvk04/details/certifications/",
+  },
+  {
+    title: "Intelligent Machining",
+    issuer: "University at Buffalo, SUNY (Coursera)",
+    link: "https://www.linkedin.com/in/pvk04/details/certifications/",
+  },
+  {
+    title: "Neural Networks and Deep Learning",
+    issuer: "Coursera (DeepLearning.AI)",
+    link: "https://www.linkedin.com/in/pvk04/details/certifications/",
+  },
+];
